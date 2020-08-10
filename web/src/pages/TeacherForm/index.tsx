a55923f54ad5d8fab566073a879cb34199384f3b
Gustavo Amorim <gustavoamorim02@gmail.com>
2020-08-10 07:12:58 -0300

    Componetize Textarea and Select

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 import PageHeader from '../../components/PageHeader'
 import Input from '../../components/Input'
+import Textarea from '../../components/Textarea'
+import Select from '../../components/Select'
 
 import warningIcon from '../../assets/images/icons/warning.svg'
 import './styles.css'
+
 
 function TeacherForm() {
   return (
@@ -20,13 +23,58 @@ function TeacherForm() {
           <Input name="name" label="Nome completo" />
           <Input name="avatar" label="Avatar" />
           <Input name="whatsapp" label="Whatsapp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
           <legend>Sobre a aula</legend>
 
-          <Input name="subject" label="Matéria" />
+          <Select 
+            name="subject" 
+            label="Matéria" 
+            options={[
+              { value: 'Português', label: 'Português' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Química', label: 'Química' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Educação Física', label: 'Educação Física' },
+              { value: 'Artes', label: 'Artes' }
+            ]}
+          />
+          
           <Input name="cost" label="Custo de sua hora por aula" />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button type="button">
+              + Novo horário
+            </button>
+          </legend>
+          
+          <div className="schedule-item">
+            <Select 
+              name="week_day" 
+              label="Dia da semana" 
+              options={[
+                { value: '0', label: 'Domingo' },
+                { value: '1', label: 'Segunda-feira' },
+                { value: '2', label: 'Terça-feira' },
+                { value: '3', label: 'Quarta-feira' },
+                { value: '4', label: 'Quinta-feira' },
+                { value: '5', label: 'Sexta-feira' },
+                { value: '6', label: 'Sábado' }
+              ]}
+            />
+
+            <Input name="from" label="Das" type="time" />
+            <Input name="to" label="Até" type="time" />
+          </div>
         </fieldset>
 
         <footer>
@@ -36,7 +84,7 @@ function TeacherForm() {
             Preencha todos os dados
           </p>
 
-          <button>Salvar cadastro</button>
+          <button type="button">Salvar cadastro</button>
         </footer>
       </main>
     </div>
